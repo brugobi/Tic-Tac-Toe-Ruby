46b0a9e2ca7fd0fd45338918614e114cb563f643
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-06-16 12:51:23 -0500

    fixing Board class errors

--- lib/player.rb
+++ lib/player.rb
@@ -1,15 +1,17 @@
 # inputs : player name(string) ** outputs:
 class Player
-  attr_accessor :name
   POSSIBILITIES = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[1, 4, 7],[2, 5, 8],[3, 6, 9],[1, 5, 9],[3, 5, 7]]
-  def  initialize(name)
+  attr_accessor :name, :picks_array
+
+  def initialize(name, picks_array = [])
     @name = name
+    @picks_array = picks_array
   end
 
   def winner?
         result = false
         POSSIBILITIES.each do |array| 
-            if array.all? { |elem| @picks_player.include?(elem)}
+            if array.all? { |elem| @picks_array.include?(elem)}
                 result = true
             end    
         end
