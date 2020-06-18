40add2d1055bf1c9912900db0ab4bca2a5b6c64b
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-06-18 11:35:55 -0500

    refactoring class Board methods code

--- lib/board.rb
+++ lib/board.rb
@@ -1,28 +1,23 @@
 # inputs : number picked(integer) and player( cross or circle) string : X or O
 # outputs : [1 , X , 3, 4, 5, 6, 7, 8, 9]
 class Board
-  @@board_grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  attr_accessor :picked_numb , :player_totem
-  def initialize (picked_numb = 0, player_totem = '')
-    @picked_numb = picked_numb
-    @player_totem = player_totem
-  end
-
-  def get_board_grid
-    @@board_grid
+  attr_accessor :grid
+  def initialize (grid = [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    @grid = grid
   end
 
   # this method change the board_grid
-  def update_grid
-    @@board_grid[@picked_numb - 1] = @player_totem
-    @@board_grid
+  def update_grid(picked_numb, player_totem)
+    @grid[picked_numb - 1] = player_totem
+    @grid
   end
 
   def grid_filled?
     result = false
-    if @@board_grid.all?(String)
+    if @grid.all?(String)
       result = true
     end
     result
   end
 end
+
