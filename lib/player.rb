6a8d04eea66de91b49bbdd1d072014190f271bb2
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-06-18 12:38:03 -0500

    refactoring the main code

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,12 @@
 # inputs : player name(string) ** outputs:
 class Player
   POSSIBILITIES = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[1, 4, 7],[2, 5, 8],[3, 6, 9],[1, 5, 9],[3, 5, 7]]
-  attr_accessor :name, :picks_array
+  attr_accessor :name, :picks_array, :totem
 
-  def initialize(name, picks_array = [])
+  def initialize(name, picks_array = [], totem)
     @name = name
     @picks_array = picks_array
+    @totem = totem
   end
 
   def winner?
