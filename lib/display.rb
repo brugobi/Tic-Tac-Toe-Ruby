6607705e2bba9484983e1bb184e22a54916a2c5e
brugobi <brgobbi@hotmail.com>
2020-06-18 12:05:04 -0400

    add pick_number, pick_not_valid and already_picked methods

--- lib/display.rb
+++ lib/display.rb
@@ -16,6 +16,15 @@ module Display
   def display_grid(grid_array)
     "\n#{grid_array[0]} | #{grid_array[1]} | #{grid_array[2]}\n---------\n#{grid_array[3]} | #{grid_array[4]} | #{grid_array[5]}\n---------\n#{grid_array[6]} | #{grid_array[7]} | #{grid_array[8]}\n"
   end
+  def pick_number(name)
+    "#{name} Pick an avalible number"
+  end  
+  def pick_not_valid
+    '\nYour pick is not valid ! pick a number from 1 to 9'
+  end 
+  def already_picked
+    '\nNumber already picked, Try again'
+  end
 end
 
 # include Display
