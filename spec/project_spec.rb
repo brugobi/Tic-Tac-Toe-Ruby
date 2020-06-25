a15760719715418af6c23d7b0532918a65454b47
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-06-25 17:24:35 -0500

    testing grid_filled? method in Board class

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,10 +3,20 @@ require './lib/player'
 require './lib/board'
 
 describe Player do
+    let (:player_test) {Player.new('X', 'player1', [1, 4, 7])}
     describe "#winner?" do
         it "winner is player 1" do
-            test = Player.new('X')
-            expect(test.winner?).to eql(false)
+            expect(player_test.winner?).to eql(true)
         end
     end
 end
+
+describe Board do
+    let (:test_board) {Board.new([1, 2, 'X'])}
+    describe "#grid_filled?" do
+        it "grid is not completely filled with strings" do
+            expect(test_board.grid_filled?).to eql(false)
+        end
+    end
+end
+
