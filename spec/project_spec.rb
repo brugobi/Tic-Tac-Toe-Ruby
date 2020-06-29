9caf745a5f002d6a169cf81894cf17c9b666eda2
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-06-29 11:50:54 -0500

    fixing Draw situation remark

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -5,12 +5,18 @@ require_relative '../lib/board'
 describe Player do
   let(:player_test1) { Player.new('X', 'player1', [1, 4, 7]) }
   let(:player_test2) { Player.new('X', 'player2', [1, 3, 7]) }
+  let(:player_test3) { Player.new('O', 'player3', [1, 3, 4]) }
   describe '#winner?' do
     it 'player2 is not a winner' do
       expect(player_test2.winner?).not_to eql(true)
     end
     it 'player 1 is a winner' do
       expect(player_test1.winner?).to eql(true)
+    end
+  end
+  context "testing a draw situation when no player is a winner" do
+    it 'it s a draw , no player is winining' do
+      expect(player_test2.winner? && player_test3.winner? ).not_to eql(true)
     end
   end
 end
